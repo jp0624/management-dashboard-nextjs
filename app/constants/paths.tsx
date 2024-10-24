b80111f75662021204feba6e840cb10f40d29249
jp0624 <jp0624@gmail.com>
2024-10-24 19:27:56 -0400

    testing paths

--- app/constants/paths.tsx
+++ app/constants/paths.tsx
@@ -1,5 +1,6 @@
-export const JSON_FILE = 'targets.json'
-export const JSON_PATH = `./data/${JSON_FILE}`
+export const DB_PATH = './public/'
 export const DB_API_PATH = './api/database'
+export const JSON_FILE = 'targets.json'
+export const JSON_PATH = `${DB_PATH}${JSON_FILE}`
 export const HISTORY_FILE = 'targets-history.json'
-export const HISTORY_PATH = `./data/${HISTORY_FILE}`
+export const HISTORY_PATH = `${DB_PATH}${HISTORY_FILE}`
