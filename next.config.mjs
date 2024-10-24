7cdb06a64d8ae8e2526f7424e4a606d942f9057e
jp0624 <jp0624@gmail.com>
2024-10-24 19:40:11 -0400

    test next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	experimental: {
+		outputFileTracingIncludes: {
+			'/': ['./data/**/*'],
+		},
+	},
+}
 
-export default nextConfig;
+export default nextConfig
